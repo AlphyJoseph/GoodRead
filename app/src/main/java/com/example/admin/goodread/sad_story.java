1c58283698510fcd5c62b78330adfde16e6e094d
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-08-07 14:06:26 +0530

    debug

--- app/src/main/java/com/example/admin/goodread/sad_story.java
+++ app/src/main/java/com/example/admin/goodread/sad_story.java
@@ -1,11 +1,7 @@
 package com.example.admin.goodread;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class sad_story extends AppCompatActivity {
 
@@ -13,11 +9,5 @@ public class sad_story extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sad_story);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-            }
-        }
-
-
+    }
+}
