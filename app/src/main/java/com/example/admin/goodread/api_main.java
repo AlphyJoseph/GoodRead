4f500d6c3279ed5f1a2e5f7bfc2ce6909ff270c1
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-09-07 20:22:27 +0530

    zzz

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -2,6 +2,8 @@ package com.example.admin.goodread;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -10,4 +12,23 @@ import java.net.URL;
 public class api_main extends AsyncTask<Void, Void, String> {
     try {
         URL url = new URL("https://api.wattpad.com:443/v4/stories?"+"Authorization"=
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            bufferedReader.close();
+            return stringBuilder.toString();
+        }
+        finally{
+            urlConnection.disconnect();
+        }
+    }
+    catch(Exception e) {
+        Log.e("ERROR", e.getMessage(), e);
+        return null;
+    }
 }
