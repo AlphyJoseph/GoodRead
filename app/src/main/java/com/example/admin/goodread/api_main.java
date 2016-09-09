71e74ea75eeb758b0ce939af2bf3631e522adc98
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-09-09 23:25:03 +0530

    main api

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -1,18 +1,23 @@
 package com.example.admin.goodread;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
  * Created by Alphy Joseph on 9/7/2016.
  */
-public class api_main extends AsyncTask<Void, Void, String> {
+public class api_main {
+    public static void apicall()
+    {
     try {
-        URL url = new URL("https://api.wattpad.com:443/v4/stories?"+"Authorization"=
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        URL url = new URL("https://api.wattpad.com:443/v4/stories?"+"Authorizarion"="COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.connect();
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
@@ -31,4 +36,5 @@ public class api_main extends AsyncTask<Void, Void, String> {
         Log.e("ERROR", e.getMessage(), e);
         return null;
     }
+}
 }
