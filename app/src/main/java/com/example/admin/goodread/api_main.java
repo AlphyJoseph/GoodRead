a50f4eeb0027fd0960cac162895b97092886efe7
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-09-11 19:39:08 +0530

    change in main api call

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -3,38 +3,37 @@ package com.example.admin.goodread;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 /**
  * Created by Alphy Joseph on 9/7/2016.
  */
-public class api_main {
-    public static void apicall()
+public class api_main{
+    public String apicall() throws ClientProtocolException,IOException
     {
-    try {
-        URL url = new URL("https://api.wattpad.com:443/v4/stories?"+"Authorizarion"="COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
-                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-                urlConnection.connect();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-            bufferedReader.close();
-            return stringBuilder.toString();
-        }
-        finally{
-            urlConnection.disconnect();
-        }
+      /*  URL url = new URL( "https://api.wattpad.com:443/v4/stories?"+ "Authorizarion"="COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();*/
+        HttpClient client= new DefaultHttpClient();
+        HttpGet request = new HttpGet();
+        request.setHeader();
+        HttpResponse response=client.execute(request);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        String line="";
+        line=bufferedReader.readLine();
+        return line;
+
+
+
     }
-    catch(Exception e) {
-        Log.e("ERROR", e.getMessage(), e);
-        return null;
     }
-}
-}
