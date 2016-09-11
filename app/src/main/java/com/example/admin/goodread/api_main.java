796e2bb8343c91f9768c6e29a5cf92e03de20d34
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-09-11 19:43:17 +0530

    bug fix

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -20,13 +20,13 @@ import java.net.URL;
  * Created by Alphy Joseph on 9/7/2016.
  */
 public class api_main{
-    public String apicall() throws ClientProtocolException,IOException
+    public String apicall(String wattpad_api) throws ClientProtocolException,IOException
     {
       /*  URL url = new URL( "https://api.wattpad.com:443/v4/stories?"+ "Authorizarion"="COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();*/
         HttpClient client= new DefaultHttpClient();
         HttpGet request = new HttpGet();
-        request.setHeader();
+        request.setHeader("Authorization","COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
         HttpResponse response=client.execute(request);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
         String line="";
