3b3bf735e81601be32593d51b58603f9d794af39
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-11-03 21:20:25 +0530

    asyc task implementation

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -26,30 +26,46 @@ import java.net.URL;
 /**
  * Created by Alphy Joseph on 9/7/2016.
  */
-public class api_main{
+
+class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
     static final String API_KEY = "COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB";
     static final String API_URL = "https://api.wattpad.com:443/v4/stories?";
+    private Exception exception;
 
-
-   /* public String apicall()
-    {
-
-        HttpClient client= new DefaultHttpClient();
-        HttpGet request = new HttpGet();
-        request.setHeader("Authorization","COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
-        HttpResponse response=client.execute(request);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line).append("\n");
+    protected void onPreExecute() {}
+    protected String doInBackground(Void... urls) {
+        try {
+            URL url = new URL(API_URL + "&apiKey=" + API_KEY);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
         }
-        bufferedReader.close();
-        return stringBuilder.toString();
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
 
 
+    }
 
-    }*/
-    };
+};
+
 
 
