7b5e70c8024d75220f39aa094ba5d252943e5311
Alphy Joseph <lizjoseph18@rocketmail.com>
2016-11-03 20:57:14 +0530

    api

--- app/src/main/java/com/example/admin/goodread/api_main.java
+++ app/src/main/java/com/example/admin/goodread/api_main.java
@@ -1,7 +1,10 @@
 package com.example.admin.goodread;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -24,19 +27,29 @@ import java.net.URL;
  * Created by Alphy Joseph on 9/7/2016.
  */
 public class api_main{
-    public String apicall() throws ClientProtocolException,IOException
+    static final String API_KEY = "COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB";
+    static final String API_URL = "https://api.wattpad.com:443/v4/stories?";
+
+
+   /* public String apicall()
     {
 
         HttpClient client= new DefaultHttpClient();
         HttpGet request = new HttpGet();
         request.setHeader("Authorization","COmAsfoTl5bHFOoHoKl8uQCo12cA8sl2ytzk2RPu3uRB");
         HttpResponse response=client.execute(request);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-        String line="";
-        line=bufferedReader.readLine();
-        return line;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        bufferedReader.close();
+        return stringBuilder.toString();
 
 
 
-    }
-    }
+    }*/
+    };
+
+
